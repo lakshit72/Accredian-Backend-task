366de0e775e42fb8ab79ed34f26e407e8e6ac5db
Lakshit Joshi <lakshitlucky11@gmail.com>
2024-07-06 22:32:26 +0530

    Error Handelling

--- index.ts
+++ index.ts
@@ -12,31 +12,44 @@ app.use(express.json());
 app.post('/refer',async (req,res)=>{
     const { email, name, PhoneNo, RefEmail } = req.body;
     try{
-        const result = await prisma.Refferees.create({
+        var result = await prisma.Refferees.create({
             data:{
                 email,
                 name,
                 PhoneNo
             }
         });
-        
-        const referrer = await prisma.User.findUnique({
+    }catch(e){
+        console.log(e);
+        res.status(500).json({
+            error:"User Already Exists"
+        })
+    }
+    try{
+
+        var referrer = await prisma.User.findUnique({
             where:{
                 email:RefEmail
             }
         })
-
-        const refRes = prisma.Reffers.create({
-            data:{
-                Refferrer:{connect:{Id:referrer.Id}},
-                Refferee:{connect:{Id:result.Id}}
-            }
-        })
-        res.json({
-            Msg:"Success",
-            Id:result.Id
-        });
     }catch(e){
+        console.log(e);
+        res.status(404).json({
+            error:"No User Exists"
+        })
+    }
+       try{
+           const refRes = prisma.Reffers.create({
+               data:{
+                   Refferrer:{connect:{Id:referrer.Id}},
+                   Refferee:{connect:{Id:result.Id}}
+                }
+            })
+            res.json({
+                Msg:"Success",
+                Id:result.Id
+            });
+        } catch(e){
         console.log(e);
         res.status(500).json({
             error:"Server Error!"
